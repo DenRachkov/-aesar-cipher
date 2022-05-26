b6ed23c834cf7628f3599cb79ecc72256d568ef2
Denis Rachkov <denis-rachkov@yandex.ru>
2022-05-26 20:50:23 +0300

    creating menu

--- src/ru/javarush/drachkov/caesarcipher/Main.java
+++ src/ru/javarush/drachkov/caesarcipher/Main.java
@@ -24,6 +24,7 @@ public class Main {
         checkingMainMenu();
 
 
+
     }
     public static void checkingMainMenu () {
         final String  SOURCE_TEXT = "Введите адрес файла с исходным текстом";
@@ -42,10 +43,12 @@ public class Main {
             case 1:
                 System.out.println(SOURCE_TEXT);
                 checkingSourceText();
+
                 break;
             case 2:
                 System.out.println(SOURCE_TEXT);
                 checkingSourceText();
+
                 break;
             case 3:
                 System.out.println(SOURCE_TEXT );
@@ -60,28 +63,57 @@ public class Main {
         }
     }
     public static Path checkingSourceText () {
+
         Scanner scanner = new Scanner(System.in);
-        String adres = scanner.nextLine();
+        String adres;
+        do {
+            adres = scanner.nextLine();
 
-        try {
-            Path path = Path.of(adres);
-            if (Files.exists(Path.of(adres))) {
 
-                if (Files.isRegularFile(Path.of(adres))){
-                    System.out.println("Файл существует");
-                } else {
-                    System.out.println("Файл не найден, попробуйте еще раз.");
+            try {
+                Path path = Path.of(adres);
+                if (Files.exists(Path.of(adres))) {
+
+                    if (Files.isRegularFile(Path.of(adres))){
+                        System.out.println("Введите ключ");
+                    } else {
+                        System.out.println("Файл не найден, попробуйте еще раз.");
+                        checkingSourceText();
+
+                    }
+                }else {
+                    System.out.println("Неверный адрес файла, попробуйте еще раз.");
+                    checkingSourceText();
 
                 }
-            }else {
-                System.out.println("Неверный адрес файла, попробуйте еще раз.");
-
+            } catch (InvalidPathException ex) {
+                System.err.println("If the path string cannot be converted to a Path");
+            } catch (SecurityException ex) {
+                System.err.println("In the case of the default provider, " +
+                        "and a security manager is installed," +
+                        " its checkRead method denies read access to the file");
             }
-        } catch (InvalidPathException ex) {
-            System.err.println("If the path string cannot be converted to a Path");
+        } while (!Files.exists(Path.of(adres)) && Files.isRegularFile(Path.of(adres)));
+        return Path.of(adres);
+    }
+    public static int checkingKey () {
+
+        Scanner scanner = new Scanner(System.in);
+        int key = 0;
+        try {
+            key = scanner.nextInt();
+            if (key >= 0 & key <= 26) {
+                System.out.println("Введите адрес файла, в который нужно записать результат.");
+            }else {
+                System.out.println("Вы ввели несоответствующий ключ, повторите попытку:");
+                checkingKey();
+            }
+        } catch (InputMismatchException ex){
+            System.err.println("Вы ввели не число, повторите попытку: ");
+            checkingKey();
 
         }
-        return Path.of(adres);
+        return key;
     }
 }
 
